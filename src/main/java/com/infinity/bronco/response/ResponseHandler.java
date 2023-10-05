7c7af14a6622dca25f7e5ec0bcd5cea9ccc243d7
rg-16 <roger.gonzales.884@gmail.com>
2023-10-04 20:17:39 -0400

    Create Response handler file

--- src/main/java/com/infinity/bronco/response/ResponseHandler.java
+++ src/main/java/com/infinity/bronco/response/ResponseHandler.java
@@ -1,2 +1,19 @@
-package com.infinity.bronco.response;public class ResponseHandler {
+package com.infinity.bronco.response;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ResponseHandler {
+
+    public static ResponseEntity<Object> generateResponse( HttpStatus status, String message, Object responseObj) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("status", status.value());
+        map.put("message", message);
+        map.put("data", responseObj);
+
+        return new ResponseEntity<Object>(map,status);
+    }
 }
