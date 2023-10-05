2e6d4a85203e5251cf44e31228806eb30bde9fda
rg-16 <roger.gonzales.884@gmail.com>
2023-10-04 20:19:33 -0400

    Create inventory repository file

--- src/main/java/com/infinity/bronco/repositories/InventoryRepository.java
+++ src/main/java/com/infinity/bronco/repositories/InventoryRepository.java
@@ -1,12 +1,11 @@
 package com.infinity.bronco.repositories;
 
-import com.infinity.bronco.models.Purchase;
+import com.infinity.bronco.models.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface PurchaseRepository extends JpaRepository<Purchase, Long> {
-    List<Purchase> findByState(int state);
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
 }
