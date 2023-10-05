7ee94245879409d319080f02c9f5f883aa0cab4a
rg-16 <roger.gonzales.884@gmail.com>
2023-10-04 20:24:21 -0400

    Create provider controler file

--- src/main/java/com/infinity/bronco/controllers/ProviderController.java
+++ src/main/java/com/infinity/bronco/controllers/ProviderController.java
@@ -1,8 +1,10 @@
 package com.infinity.bronco.controllers;
 
 import com.infinity.bronco.models.Provider;
+import com.infinity.bronco.response.ResponseHandler;
 import com.infinity.bronco.services.ProviderService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,27 +19,77 @@ public class ProviderController {
     private final ProviderService providerService;
 
     @PostMapping
-    public ResponseEntity<Provider> createProvider(@RequestBody Provider provider) {
-        return ResponseEntity.ok(providerService.saveProvider(provider));
+    public ResponseEntity<Object> createProvider(@RequestBody Provider provider) {
+        try {
+            Provider result = providerService.saveProvider(provider);
+            if (result != null)
+            {
+                return ResponseHandler.generateResponse( HttpStatus.CREATED, "Proveedor creado con Exito!", result);
+            }else {
+                return ResponseHandler.generateResponse( HttpStatus.BAD_REQUEST, "[Register Error:], Proveedor no registrado", null);
+            }
+
+        } catch (Exception e) {
+            return ResponseHandler.generateResponse(HttpStatus.MULTI_STATUS, e.getMessage(), null);
+        }
     }
 
     @GetMapping(path = "/{id}")
-    public ResponseEntity<Optional<Provider>> findById(@PathVariable("id") Long providerId) {
-        return ResponseEntity.ok(providerService.getProviderById(providerId));
+    public ResponseEntity<Object> findById(@PathVariable("id") Long providerId) {
+        try {
+            Optional<Provider> result = providerService.getProviderById(providerId);
+            return ResponseHandler.generateResponse( HttpStatus.OK, "Proveedor encontrado con ID: "+providerId, result);
+
+        } catch (Exception e) {
+            return ResponseHandler.generateResponse(HttpStatus.MULTI_STATUS, e.getMessage(), null);
+        }
     }
 
     @GetMapping
-    public ResponseEntity<Iterable<Provider>> getAllProviders() {
-        return ResponseEntity.ok(providerService.getProviders());
+    public ResponseEntity<Object> getAllProviders() {
+        try {
+            Iterable<Provider> result = providerService.getProviders();
+            if (result != null)
+            {
+                return ResponseHandler.generateResponse( HttpStatus.OK, "[Exito:], Proveedores encontrados", result);
+            }else {
+                return ResponseHandler.generateResponse( HttpStatus.BAD_REQUEST, "[Get Error:], Proveedores no encontrados", null);
+            }
+
+        } catch (Exception e) {
+            return ResponseHandler.generateResponse(HttpStatus.MULTI_STATUS, e.getMessage(), null);
+        }
     }
 
     @PutMapping(path = "/{id}")
-    public ResponseEntity<Provider> updateProvider(@PathVariable("id") Long id, @RequestBody Provider provider){
-        return ResponseEntity.ok(providerService.updateProviderById(id, provider));
+    public ResponseEntity<Object> updateProvider(@PathVariable("id") Long id, @RequestBody Provider provider){
+        try {
+            Provider result = providerService.updateProviderById(id, provider);
+            if (result != null)
+            {
+                return ResponseHandler.generateResponse( HttpStatus.OK, "[Exito:], El Proveedor fue actualizado", result);
+            }else {
+                return ResponseHandler.generateResponse( HttpStatus.NOT_FOUND, "[Update Error:], El Proveedor no fue actualizado", null);
+            }
+
+        } catch (Exception e) {
+            return ResponseHandler.generateResponse(HttpStatus.BAD_REQUEST, e.getMessage(), null);
+        }
     }
 
     @DeleteMapping(path = "/{id}")
-    public ResponseEntity<Provider> deleteProvider(@PathVariable("id") Long id) {
-        return ResponseEntity.ok(providerService.deleteProvider(id));
+    public ResponseEntity<Object> deleteProvider(@PathVariable("id") Long id) {
+        try {
+            Provider result = providerService.deleteProvider(id);
+            if (result != null)
+            {
+                return ResponseHandler.generateResponse( HttpStatus.OK, "[Exito:], El Proveedor fue eliminado", result);
+            }else {
+                return ResponseHandler.generateResponse( HttpStatus.NOT_FOUND, "[Delete Error:], El Proveedor no fue eliminado", null);
+            }
+
+        } catch (Exception e) {
+            return ResponseHandler.generateResponse(HttpStatus.BAD_REQUEST, e.getMessage(), null);
+        }
     }
 }
