3380c03e107ce6e7386b049d117ebbd61534621c
rg-16 <roger.gonzales.884@gmail.com>
2023-10-04 20:13:24 -0400

    Create purchase detail service

--- src/main/java/com/infinity/bronco/services/PurchaseDetailService.java
+++ src/main/java/com/infinity/bronco/services/PurchaseDetailService.java
@@ -1,52 +1,24 @@
 package com.infinity.bronco.services;
 
-import com.infinity.bronco.models.Purchase;
-import com.infinity.bronco.repositories.PurchaseRepository;
+import com.infinity.bronco.models.PurchaseDetail;
+import com.infinity.bronco.repositories.PurchaseDetailRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 @AllArgsConstructor
-public class PurchaseService {
+public class PurchaseDetailService {
 
 
-    private final PurchaseRepository purchaseRepository;
+    private final PurchaseDetailRepository purchaseDetailRepository;
 
-   public Iterable<Purchase> getPurchases() {
-        return purchaseRepository.findByState(1);
+   public Iterable<PurchaseDetail> getPurchaseDetails() {
+        return purchaseDetailRepository.findAll();
     }
 
-    public Optional<Purchase> getPurchaseById(Long id) {
-        return purchaseRepository.findById(id);
+    public PurchaseDetail savePurchaseDetail(PurchaseDetail purchaseDetail) {
+
+       return purchaseDetailRepository.save(purchaseDetail);
     }
 
-    public Purchase savePurchase(Purchase purchase) {
-        if (purchaseRepository.existsById(purchase.getIdPurchase())) {
-            purchase.setIdPurchase(null);
-        }
-        return purchaseRepository.save(purchase);
-    }
-
-    public Purchase  updatePurchaseById(Long id, Purchase purchase){
-        Purchase updatePurchase = purchaseRepository.findById(id)
-                .orElseThrow();
-
-        updatePurchase.setTotal(purchase.getTotal());
-        updatePurchase.setDate(purchase.getDate());
-        updatePurchase.setProvider(purchase.getProvider());
-
-        purchaseRepository.save(updatePurchase);
-
-        return purchaseRepository.save(updatePurchase);
-    }
-
-    public Purchase deletePurchase(Long id) {
-        Purchase removePurchase = purchaseRepository.findById(id)
-                .orElseThrow();
-        removePurchase.setState((byte) 0);
-        purchaseRepository.save(removePurchase);
-        return purchaseRepository.save(removePurchase);
-    }
 }
