221cc503a60be3f05df86eac1a37c856fe831738
rg-16 <roger.gonzales.884@gmail.com>
2023-10-04 20:16:54 -0400

    Create inventory file service.

--- src/main/java/com/infinity/bronco/services/InventoryService.java
+++ src/main/java/com/infinity/bronco/services/InventoryService.java
@@ -1,7 +1,7 @@
 package com.infinity.bronco.services;
 
-import com.infinity.bronco.models.Purchase;
-import com.infinity.bronco.repositories.PurchaseRepository;
+import com.infinity.bronco.models.Inventory;
+import com.infinity.bronco.repositories.InventoryRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,44 +9,45 @@ import java.util.Optional;
 
 @Service
 @AllArgsConstructor
-public class PurchaseService {
+public class InventoryService {
 
 
-    private final PurchaseRepository purchaseRepository;
+    private final InventoryRepository inventoryRepository;
 
-   public Iterable<Purchase> getPurchases() {
-        return purchaseRepository.findByState(1);
+   public Iterable<Inventory> getInventories() {
+        return inventoryRepository.findAll();
     }
 
-    public Optional<Purchase> getPurchaseById(Long id) {
-        return purchaseRepository.findById(id);
+    public Optional<Inventory> getInventoryById(Long id) {
+        return inventoryRepository.findById(id);
     }
 
-    public Purchase savePurchase(Purchase purchase) {
-        if (purchaseRepository.existsById(purchase.getIdPurchase())) {
-            purchase.setIdPurchase(null);
+    public Inventory saveInventory(Inventory inventory) {
+        if (inventoryRepository.existsById(inventory.getId())) {
+            inventory.setId(null);
         }
-        return purchaseRepository.save(purchase);
+        return inventoryRepository.save(inventory);
     }
 
-    public Purchase  updatePurchaseById(Long id, Purchase purchase){
-        Purchase updatePurchase = purchaseRepository.findById(id)
+    public Inventory  updateInventoryById(Long id, Inventory inventory){
+        Inventory updateInventory = inventoryRepository.findById(id)
                 .orElseThrow();
 
-        updatePurchase.setTotal(purchase.getTotal());
-        updatePurchase.setDate(purchase.getDate());
-        updatePurchase.setProvider(purchase.getProvider());
+        updateInventory.setLot(inventory.getLot());
+        updateInventory.setPrice(inventory.getPrice());
+        updateInventory.setStock(inventory.getStock());
+        updateInventory.setProduct(inventory.getProduct());
 
-        purchaseRepository.save(updatePurchase);
+        inventoryRepository.save(updateInventory);
 
-        return purchaseRepository.save(updatePurchase);
+        return inventoryRepository.save(updateInventory);
     }
 
-    public Purchase deletePurchase(Long id) {
-        Purchase removePurchase = purchaseRepository.findById(id)
+    public Inventory deleteInventory(Long id) {
+        Inventory removeInventory = inventoryRepository.findById(id)
                 .orElseThrow();
-        removePurchase.setState((byte) 0);
-        purchaseRepository.save(removePurchase);
-        return purchaseRepository.save(removePurchase);
+        removeInventory.setState((byte) 0);
+        inventoryRepository.save(removeInventory);
+        return inventoryRepository.save(removeInventory);
     }
 }
