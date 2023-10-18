e1585a3591a501e1aca54834493973e25287c4e7
rg-16 <roger.gonzales.884@gmail.com>
2023-10-18 02:13:53 -0400

    Create purchase service file.

--- src/main/java/com/infinity/bronco/services/PurchaseService.java
+++ src/main/java/com/infinity/bronco/services/PurchaseService.java
@@ -1,12 +1,19 @@
 package com.infinity.bronco.services;
 
+import com.infinity.bronco.models.Inventory;
+import com.infinity.bronco.models.Provider;
 import com.infinity.bronco.models.Purchase;
+import com.infinity.bronco.models.PurchaseDetail;
+import com.infinity.bronco.repositories.InventoryRepository;
+import com.infinity.bronco.repositories.PurchaseDetailRepository;
 import com.infinity.bronco.repositories.PurchaseRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 @AllArgsConstructor
@@ -14,9 +21,11 @@ public class PurchaseService {
 
 
     private final PurchaseRepository purchaseRepository;
+    private final PurchaseDetailRepository purchaseDetailRepository;
+    private final InventoryRepository inventoryRepository;
 
    public Iterable<Purchase> getPurchases() {
-        return purchaseRepository.findAll();
+        return purchaseRepository.findByState(1);
     }
 
     public Optional<Purchase> getPurchaseById(Long id) {
@@ -25,10 +34,33 @@ public class PurchaseService {
 
     @Transactional
     public Purchase savePurchase(Purchase purchase) {
-        if (purchaseRepository.existsById(purchase.getId())) {
-            purchase.setId(null);
+
+        Purchase purchasePersist = new Purchase();
+        PurchaseDetail purchaseDetailPersist = new PurchaseDetail();
+
+        //CREATE PURCHASE OBJECT
+        purchasePersist.setDate(purchase.getDate());
+        purchasePersist.setTotal(purchase.getTotal());
+        purchasePersist.setProvider(purchase.getProvider());
+
+        Purchase purchase1 = purchaseRepository.save(purchasePersist);
+
+
+        for (PurchaseDetail purchaseDetail : purchase.getPurchaseDetails()) {
+            purchaseDetailPersist.setId(0);
+            purchaseDetailPersist.setPurchase(purchase1);
+            purchaseDetailPersist.setProduct(purchaseDetail.getProduct());
+            purchaseDetailPersist.setAmount(purchaseDetail.getAmount());
+            purchaseDetailPersist.setPrice(purchaseDetail.getPrice());
+            purchaseDetailPersist.setQuantity(purchaseDetail.getQuantity());
+            Inventory inventory = inventoryRepository.save(purchaseDetail.getInventory());
+            purchaseDetailPersist.setInventory(inventory);
+
+            PurchaseDetail p = purchaseDetailRepository.save(purchaseDetailPersist);
         }
-        return purchaseRepository.save(purchase);
+
+        return purchasePersist;
+
     }
 
     public Purchase  updatePurchaseById(Long id, Purchase purchase){
